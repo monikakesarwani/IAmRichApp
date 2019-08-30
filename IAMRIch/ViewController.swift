c6b8b1945f3e8b953ef0058760202c5b06b3a84e
Monika <moni.kesarwani@yahoo.com>
2019-08-29 22:58:05 -0400

    file uploaded

---
+++ IAMRIch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IAMRIch
+//
+//  Created by Anil on 8/20/19.
+//  Copyright © 2019 kesarwani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
